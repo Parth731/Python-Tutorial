459f70a27bdd9304ad8ec9a80dfea64f8cc762f2
ADMIN <patelparth31795@gmail.com>
2020-08-20 18:15:36 +0530

    Comments Escape sequances & print tut6

---
+++ 7_Varible_Datatype_Typecasting.py
@@ -0,0 +1,42 @@
+
+var1 = "54 "
+var4 = "32"
+var2 = 4
+var3 = 36.7
+print(var1)
+print(var2)
+print(var3)
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+# print(var1 + var3)#error
+print(var2+var3)
+print(var1 + var4)
+
+# take input from user
+print("Enter your number")
+impnum = input()
+
+# print("You enter ",impnum + 10) #error
+print("You enter ",int(impnum) + 10) #88
+
+
+
+# typecasting
+"""
+str()
+int()
+float()
+"""
+'''
+Quiz - sloved in the video
+exercise - next video
+project - 
+'''
+
+# print(10 * str(int(var1) + int(var4)))
+# print(float(var1) + float(var4))
+#
+# print("Hello World\n"*10)
---
+++ Quiz/1_Quiz.py
@@ -0,0 +1,5 @@
+
+n1 = int(input("Enter the number 1\n"))
+n2 = int(input("Enter the number 2\n"))
+
+print("sum of these two number",n1+n2);
